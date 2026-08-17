aca557108e9105de143035fe1c16daa08c2ec66b
Pedro <phmundel2009@gmail.com>
2026-08-17 08:31:42 -0300

    Login bonito

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "../styles/Login.module.css"
+import logo from "../assets/LogoDark.png"
 
 function Login() {
   const [codigo, setCodigo] = useState("")  
@@ -19,28 +21,38 @@ function Login() {
   }
 
   return (
-    <form onSubmit={handleLogin}>
-      <h2>Login</h2>
-      <input
-        type="Number"
-        placeholder="Código da Empresa"
-        value={codigo}
-        onChange={(e) => setCodigo(e.target.value)}
-      />
-      <input
-        type="Number"
-        placeholder="Matriucla"
-        value={usuario}
-        onChange={(e) => setUsuario(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Senha Pessoal"
-        value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-      />
-      <button type="submit">Entrar</button>
-    </form>
+
+    <>
+    <div className={styles.pagina}>
+      <img src={logo} alt="logo" className={styles.Logo}/>
+
+      <form className={styles.container} id="LoginDiv" onSubmit={handleLogin}>
+        <h2>Login</h2>
+        <input
+          className={styles.codigo}
+          type="Number"
+          placeholder="Código da Empresa"
+          value={codigo}
+          onChange={(e) => setCodigo(e.target.value)}
+        />
+        <input
+          className={styles.codigo}
+          type="Number"
+          placeholder="Matriucla"
+          value={usuario}
+          onChange={(e) => setUsuario(e.target.value)}
+        />
+        <input
+          className={styles.codigo}
+          type="password"
+          placeholder="Senha Pessoal"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        <button type="submit" className={styles.botao}>Entrar</button>
+      </form>
+    </div>
+    </>
   );
 }
 
